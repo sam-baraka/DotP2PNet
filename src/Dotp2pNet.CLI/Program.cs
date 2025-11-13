e6dae909acf40325d5dd5c6143fe84ccc233b430
Samuel Baraka <sababuvercetti@gmail.com>
2025-11-13 09:47:13 +0300

    feat: integrate message framing demo into CLI application
    - Add Networking project reference to CLI
    - Update Program.cs to run MessageFramingDemo
    - Provides immediate validation of message framing implementation
    - Demonstrates TCP stream handling and message parsing in action
    - Educational tool showing how length-prefix protocol works

--- src/Dotp2pNet.CLI/Program.cs
+++ src/Dotp2pNet.CLI/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Dotp2pNet.Networking.Framing;
+
+Console.WriteLine("Dotp2pNet - Message Framing Layer Demo\n");
+
+// Run all demonstrations
+MessageFramingDemo.RunAll();
+
+Console.WriteLine("\nPress any key to exit...");
+Console.ReadKey();
